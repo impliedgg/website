f7fc9931924949d6d2ee903ba5caf6acbf8078b0
emily <58041173+impliedgg@users.noreply.github.com>
2025-01-29 20:18:20 -0600

    opengraph, meta description, and bump deps

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,10 @@
-<svelte:head>
-    <title>emily's site</title>
-    
-</svelte:head>
+<script>
 
-<style>
-    h1 {
-        margin-bottom: 0;
-    }
-    h1 + p {
-        color: var(--layer-10);
-        margin-top: 0;
-    }
-</style>
+</script>
+
+<svelte:head>
+    <meta name="description" content="emily's personal website. find my portfolio, contacts, and a biography.">
+</svelte:head>
 
 
 <h1>hi, i'm emily</h1>
@@ -23,3 +16,13 @@
 <p>
     this site is a bit of a stub&mdash;it's still w.i.p. as i need to improve my resume and portfolio still but im too busy :p
 </p>
+
+<style>
+    h1 {
+        margin-bottom: 0;
+    }
+    h1 + p {
+        color: var(--layer-10);
+        margin-top: 0;
+    }
+</style>
